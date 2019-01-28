7cc4a2d59e1aacb1e79ef1a7d2d1107ec469a71e
Adrien <winy95battle@gmail.com>
2019-01-28 19:04:41 +0100

    Add env var that point on config

--- source/main.py
+++ source/main.py
@@ -1,22 +1,25 @@
 import ConfigParser
 
+
 class Config(object):
-    def __init__(self):
-        pass
     def add(self, name, section):
         setattr(self, name, section)
+
 
 class Section(object):
     def __init__(self, values):
         for name, value in values:
             setattr(self, name, value)
 
+
 if __name__ == '__main__':
+    import os
+    env = os.getenv("APP_ENV", "dev")
+    print(env)
     config_parser = ConfigParser.ConfigParser()
-    config_parser.read("../config/dev.ini")
+    config_parser.read("config/dev.ini")
 
     config = Config()
     for section_name in config_parser.sections():
         section = Section(config_parser.items(section_name))
         config.add(section_name, section)
-
